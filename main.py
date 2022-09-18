ebfb1208f2721b82a06bf8e991f5490e514cd6a1
Matt <matttmlive@gmail.com>
2022-09-18 03:24:51 +0100

    Add cache file

--- main.py
+++ main.py
@@ -4,15 +4,27 @@ import requests
 import json
 import time
 
-with open("config.json") as config:
-    config = json.load(config)
+try:
+    with open("config.json") as config:
+        config = json.load(config)
+except:
+    raise FileNotFoundError("You have not created the config file under the name 'config.json'")
 
-# first run, there is no 'cache'
-cache = ""
+# first run, there is no 'cache' unless cache file is stored
+if config['cache']:
+    try:
+        with open("cache.rss") as cache_file:
+            cache = cache_file.read()
+    except Exception as e:
+        print("Error loading cache file.")
+        cache = ""
+else:
+    cache = ""
 
+# loop to check for new rss feed (essentially a listener)
 while True:
     newsfeed = feedparser.parse(config['rss_feed_url']).entries
-    if cache == newsfeed:
+    if cache == str(newsfeed):
         pass # there has been no update to the RSS feed compared to the cache so nothing will be done
     else:
         # get first section of summary for embed
@@ -36,7 +48,7 @@ while True:
             "title": f":newspaper: | {title}",
             "description": output,
             "url": link,
-            "color": 5814783
+            "color": 0xad2f26
             }
         ],
         "username": config['discord_webhook_username'],
@@ -44,4 +56,7 @@ while True:
         })
         # set the 'cache' to the latest newsfeed obtained
         cache = newsfeed
+        if config['cache']:
+            with open('cache.rss', 'w+') as cache_file:
+                cache_file.write(str(cache))
     time.sleep(config['check_interval']) # checks every x seconds (set in config)
